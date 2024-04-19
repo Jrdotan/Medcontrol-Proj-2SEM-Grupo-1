ea736a7939c15d2d14455904021c5fd9cf153ccf
Pedro Rufino <pedro.rmn19@gmail.com>
2024-04-19 14:31:24 -0300

    [Feat]: Inplementando os primeiros códigos do projeto

---
+++ index.php
@@ -0,0 +1,23 @@
+<!-- <head> -->
+<?php include './includes/base/headT.php' ?>
+<title>MedControl</title>
+<link rel="stylesheet" href="./assets/css/index.css">
+<?php include './includes/base/headF.php' ?>
+<!-- </head> -->
+
+<!-- <body> -->
+<?php include './includes/base/topo.php' ?>
+<nav class="navbar bg-body-tertiary size-nav shadow-MC">
+    <div class="container-fluid">
+        <a class="navbar-brand m-0 nav-title" href="#">
+            <img src="./assets/img/Logo.svg" alt="Logo" width="75" height="83" class="d-inline-block">
+            <span>MedControl</span>
+        </a>
+        <form class="d-flex m-0" role="search">
+            <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+            <button class="btn btn-outline-success" type="submit">Search</button>
+        </form>
+    </div>
+</nav>
+<?php include './includes/base/base.php' ?>
+<!-- </body> -->
