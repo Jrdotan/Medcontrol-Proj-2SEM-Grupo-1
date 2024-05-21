917b192ca3571e9b1e5a545f9781374450457fdd
Felipe-candido <felipepcandido8@gmail.com>
2024-05-20 21:24:40 -0300

    Modelo físico do banco atualizado

--- MedControl/database/mdcontrol_db_v2.sql
+++ MedControl/database/mdcontrol_db_v2.sql
@@ -1,17 +1,43 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Tempo de geração: 21-Maio-2024 às 02:19
+-- Versão do servidor: 10.4.32-MariaDB
+-- versão do PHP: 8.2.12
+
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
 SET time_zone = "+00:00";
+
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
 
+--
+-- Banco de dados: `mdcontrol_db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `doencas`
+--
 
 CREATE TABLE `doencas` (
   `ID` int(11) NOT NULL,
   `nome` varchar(255) DEFAULT NULL,
   `CID` varchar(30) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `funcionario`
+--
 
 CREATE TABLE `funcionario` (
   `ID` int(11) NOT NULL,
@@ -22,9 +48,14 @@ CREATE TABLE `funcionario` (
   `senha` longtext NOT NULL,
   `cargo` varchar(30) DEFAULT NULL,
   `date_ini` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
-  `sexo` enum('F','M','Outros') DEFAULT NULL,
   `CRM` int(7) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `paciente`
+--
 
 CREATE TABLE `paciente` (
   `ID` int(11) NOT NULL,
@@ -38,6 +69,12 @@ CREATE TABLE `paciente` (
   `telefone` varchar(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `prontuario`
+--
+
 CREATE TABLE `prontuario` (
   `ID` int(11) NOT NULL,
   `ID_paciente` int(11) DEFAULT NULL,
@@ -48,42 +85,79 @@ CREATE TABLE `prontuario` (
   `ID_funcionario` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Índices para tabelas despejadas
+--
 
+--
+-- Índices para tabela `doencas`
+--
 ALTER TABLE `doencas`
   ADD PRIMARY KEY (`ID`);
 
+--
+-- Índices para tabela `funcionario`
+--
 ALTER TABLE `funcionario`
   ADD PRIMARY KEY (`ID`);
 
+--
+-- Índices para tabela `paciente`
+--
 ALTER TABLE `paciente`
   ADD PRIMARY KEY (`ID`),
   ADD UNIQUE KEY `CPF` (`CPF`),
   ADD UNIQUE KEY `email` (`email`);
 
+--
+-- Índices para tabela `prontuario`
+--
 ALTER TABLE `prontuario`
   ADD PRIMARY KEY (`ID`),
   ADD KEY `FK_Paciente` (`ID_paciente`),
   ADD KEY `FK_Funcionario` (`ID_funcionario`),
   ADD KEY `FK_Doenca` (`ID_doenca`);
 
+--
+-- AUTO_INCREMENT de tabelas despejadas
+--
 
+--
+-- AUTO_INCREMENT de tabela `doencas`
+--
 ALTER TABLE `doencas`
   MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
 
+--
+-- AUTO_INCREMENT de tabela `funcionario`
+--
 ALTER TABLE `funcionario`
   MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
 
+--
+-- AUTO_INCREMENT de tabela `paciente`
+--
 ALTER TABLE `paciente`
   MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
 
+--
+-- AUTO_INCREMENT de tabela `prontuario`
+--
 ALTER TABLE `prontuario`
   MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
 
+--
+-- Restrições para despejos de tabelas
+--
 
+--
+-- Limitadores para a tabela `prontuario`
+--
 ALTER TABLE `prontuario`
   ADD CONSTRAINT `FK_Doenca` FOREIGN KEY (`ID_doenca`) REFERENCES `doencas` (`ID`),
   ADD CONSTRAINT `FK_Funcionario` FOREIGN KEY (`ID_funcionario`) REFERENCES `funcionario` (`ID`),
   ADD CONSTRAINT `FK_Paciente` FOREIGN KEY (`ID_paciente`) REFERENCES `paciente` (`ID`);
+COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
