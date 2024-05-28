38660d51bf950919199d22886617598784e6bd87
Pedro Rufino <pedro.rmn19@gmail.com>
2024-05-27 23:50:33 -0300

    [Feat]: adicionando CIDs na tela incial

---
+++ MedControl/pages/includes/selects.php
@@ -0,0 +1,30 @@
+<?php
+class DBSelects extends medcontrol_db {
+    public function select_all_cids() {
+        try {
+            $result = $this->connect()->query("SELECT * FROM doencas");
+            if ($result->rowCount() > 0) {
+                $cout = 1;
+                while ($cid = $result->fetch(PDO::FETCH_ASSOC)) {
+                    echo '
+                    <li class="list-group-item">
+                        <div class="form-check">
+                            <input class="form-check-input" name="'.$cid["CID"].'" type="radio" name="exampleRadios" id="exampleRadios'.$cout.'">
+                            <label class="form-check-label" for="exampleRadios'.$cout.'">
+                                '.$cid["nome"].'
+                            </label>
+                        </div>
+                    </li>';
+                    $cout ++;
+                }
+            } else {
+                echo "Nenhum doença encontrada.";
+            }
+        } catch (PDOException $error){
+            echo "Erro na consulta: " . $error->getMessage();
+            return false;
+        }
+    }
+}
+
+?>
